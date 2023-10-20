4868a5b1e8f24fa8bc48ae18bbc4749923982cc8
Lakrids7 <Krelle757@gmail.com>
2023-10-20 14:15:47 +0200

    NPC head rotation er gradual + fixed animation delay
    NPC'er kigger stadig lidt ned af når man går tæt på dem (Både i og ude af VR), og tror måden vi kan fikse det her på er at få dem til at fokusere på camera i stedet for player objectet

--- VR_Game/Assets/Scripts/NpcAnimation.cs
+++ VR_Game/Assets/Scripts/NpcAnimation.cs
@@ -16,6 +16,8 @@ public class NpcAnimation : MonoBehaviour
     [SerializeField] private GameObject headAim;
     private MultiAimConstraint _multiAimConstraint;
     
+    private Coroutine weightChangeCoroutine;  // To store the coroutine reference
+    
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -56,7 +58,14 @@ public class NpcAnimation : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            _multiAimConstraint.weight = 1;
+            // If the coroutine is already running, stop it before starting a new one
+            if (weightChangeCoroutine != null)
+            {
+                StopCoroutine(weightChangeCoroutine);
+            }
+
+            // Start a new coroutine to gradually change the weight to 1 over 2 seconds
+            weightChangeCoroutine = StartCoroutine(ChangeWeightOverTime(1.0f, 1.0f));
         }
     }
 
@@ -64,8 +73,34 @@ public class NpcAnimation : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            _multiAimConstraint.weight = 0;
-            
+            // If the coroutine is already running, stop it before starting a new one
+            if (weightChangeCoroutine != null)
+            {
+                StopCoroutine(weightChangeCoroutine);
+            }
+
+            // Start a new coroutine to gradually change the weight to 0 over 2 seconds
+            weightChangeCoroutine = StartCoroutine(ChangeWeightOverTime(0.0f, 1.0f));
         }
+    }
+
+    private IEnumerator ChangeWeightOverTime(float targetWeight, float duration)
+    {
+        float startTime = Time.time;
+        float startWeight = _multiAimConstraint.weight;
+
+        while (Time.time - startTime < duration)
+        {
+            float elapsedTime = Time.time - startTime;
+            float progress = Mathf.Clamp01(elapsedTime / duration);
+            _multiAimConstraint.weight = Mathf.Lerp(startWeight, targetWeight, progress);
+            yield return null;
+        }
+
+        // Ensure the weight is set to the target value at the end
+        _multiAimConstraint.weight = targetWeight;
+
+        // Clear the coroutine reference
+        weightChangeCoroutine = null;
     }
 }
