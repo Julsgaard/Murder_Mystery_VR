4ecefb052afc5ac4e3f754b57d2b6bc172958a6b
Julsgaard <100912975+Julsgaard@users.noreply.github.com>
2024-08-09 01:11:20 +0200

    Fixed some warnings
    Also updated Rider package

--- VR_Game/Assets/Scripts/GameManager.cs
+++ VR_Game/Assets/Scripts/GameManager.cs
@@ -63,33 +63,37 @@ public class GameManager : MonoBehaviour
         if (enableVR && !enableDialogueOptions)
         {
             selectedMicrophone = Microphone.devices[0]; // Select the first microphone in the list of available microphones
-            return;
             
-            
-            // This code selects the Oculus microphone, but the oculus microphone creates a lag spike when starting and ending recording (might need it later)
-            string oculusMic = "Headset Microphone (Oculus Virtual Audio Device)";
-            bool oculusMicFound = false;
-
-            // Check if Oculus microphone is available
-            foreach (var mic in availableMicrophones)
-            {
-                if (mic == oculusMic)
-                {
-                    selectedMicrophone = oculusMic;
-                    oculusMicFound = true;
-                    break;
-                }
-            }
-            
-            // If Oculus microphone is not found, use the default microphone
-            if (!oculusMicFound)
-            {
-                Debug.LogWarning("No Quest 2 microphone found. Using default microphone instead.");
-                selectedMicrophone = Microphone.devices[0]; // Select the first microphone in the list of available microphones
-            }
+            // This method selects the Oculus microphone, but the oculus microphone creates a lag spike when starting and ending recording (might need it later)
+            //UseOculusMicrophone();
         }
         else
         {
+            selectedMicrophone = Microphone.devices[0]; // Select the first microphone in the list of available microphones
+        }
+    }
+
+    private void UseOculusMicrophone()
+    {
+        // This code selects the Oculus microphone, but the oculus microphone creates a lag spike when starting and ending recording (might need it later)
+        string oculusMic = "Headset Microphone (Oculus Virtual Audio Device)";
+        bool oculusMicFound = false;
+
+        // Check if Oculus microphone is available
+        foreach (var mic in availableMicrophones)
+        {
+            if (mic == oculusMic)
+            {
+                selectedMicrophone = oculusMic;
+                oculusMicFound = true;
+                break;
+            }
+        }
+            
+        // If Oculus microphone is not found, use the default microphone
+        if (!oculusMicFound)
+        {
+            Debug.LogWarning("No Quest 2 microphone found. Using default microphone instead.");
             selectedMicrophone = Microphone.devices[0]; // Select the first microphone in the list of available microphones
         }
     }
--- VR_Game/Assets/Scripts/ObjectInfo.cs
+++ VR_Game/Assets/Scripts/ObjectInfo.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 public class ObjectInfo : MonoBehaviour
 {
     public Vector3 originalScale;
